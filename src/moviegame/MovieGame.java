499f78f2736f82e5cb21dd998fee8ae40ef5392d
stocknat <stocknat@011-4303-S30BE.students.cisd.conroeisd.loc>
2018-11-30 09:01:55 -0600

    first commit

---
+++ src/moviegame/MovieGame.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moviegame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author stocknat
+ */
+public class MovieGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        The rules are simple, the computer randomly picks a movie title, and 
+//        shows you how many letters it's made up of. Your goal is to try to 
+//        figure out the movie by guessing one letter at a time. If a letter is 
+//        indeed in the title the computer will reveal its correct position in 
+//        the word, if not, you lose a point. If you lose 10 points, game over!
+//        BUT the more correct letters you guess the more obvious the movie 
+//        becomes and at a certain point you should be able to figure it out.
+//        The program will randomly pick a movie title from a text file that 
+//        contains a large list of movies. You can download a sample text file  
+//        to play with from the resources tab or create your own list of movie 
+//        titles. Once the computer picks a random title, it will display 
+//        underscores _ in place of the real letters, thereby only giving away 
+//        the number of letters in the movie title.
+        
+        
+        
+    }
+}
